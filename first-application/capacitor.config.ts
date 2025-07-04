5610967f5ee7bbb28dc2675ebbe94324b4c6ba30
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-04 00:01:59 -0300

    remove: Deleted project

--- first-application/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'first-application',
-  webDir: 'www'
-};
-
-export default config;
