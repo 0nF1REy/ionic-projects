135755732b532a72c3c6c59051949e4811929b93
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-04 00:00:54 -0300

    feat: Rewording

---
+++ texto.sh
@@ -0,0 +1,24 @@
+# Instala a CLI do Ionic (versão 7) globalmente
+npm i -g @ionic/cli@7
+
+# Mostra informações do ambiente
+ionic info
+
+# Cria um novo projeto Ionic
+ionic start
+
+# Use the app creation wizard? - NO
+# Pick a framework! - ANGULAR
+# Every great app needs a name! - NOME-DO-PROGRAMA
+# Starter template - BLANK
+# Build with Standalone Components or NgModules? - STANDALONE
+# Create free Ionic account? - NO
+
+# inicia o servidor de desenvolvimento
+ionic s                    
+
+# Gera página 'receita' na pasta pages
+ionic g page pages/receita
+
+# Gera página 'cadastro' na pasta pages
+ionic g page pages/cadastro
